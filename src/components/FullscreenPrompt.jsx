a42def7508904116dacbe046a3e74c4f89883e13
krnelpan1c <castilloi8160@gmail.com>
2026-01-28 20:31:39 -0800

    add more info to fullscreen prompt

--- src/components/FullscreenPrompt.jsx
+++ src/components/FullscreenPrompt.jsx
@@ -44,7 +44,7 @@ const FullscreenPrompt = ({ onDismiss }) => {
                 <div>
                     <h3 className="text-xl font-bold text-white mb-2">Immersive Mode</h3>
                     <p className="text-white/70 text-sm leading-relaxed">
-                        Press <kbd className="px-2 py-1 bg-white/10 rounded border border-white/20 text-white font-mono text-xs">Fullscreen</kbd> or <kbd className="px-2 py-1 bg-white/10 rounded border border-white/20 text-white font-mono text-xs">F11</kbd> and then switch to another app to put the wallpaper in the background.
+                        Press <kbd className="px-2 py-1 bg-white/10 rounded border border-white/20 text-white font-mono text-xs">Fullscreen</kbd> or <kbd className="px-2 py-1 bg-white/10 rounded border border-white/20 text-white font-mono text-xs">F11</kbd> and then switch to another app to put the wallpaper in the background. <br></br><br></br> Tip: Press <kbd className="px-2 py-1 bg-white/10 rounded border border-white/20 text-white font-mono text-xs">esc</kbd> while the wallpaper is focused to return to the wallpaper picker.
                     </p>
                 </div>
 
